97badae0ba4bf60aa5fecd8eae6953391d1f0f88
EliChin <elizaveta@nspcc.ru>
2021-04-19 19:05:36 +0300

    Variables put in common.py

--- robot/variables/common.py
+++ robot/variables/common.py
@@ -5,6 +5,8 @@ ROOT='../..'
 
 RESOURCES="%s/resources/lib" % ROOT
 CERT="%s/../../ca" % ROOT
+
+os.environ
 # path from repo root is required for object put and get
 # in case when test is run from root in docker
 ABSOLUTE_FILE_PATH="/robot/testsuites/integration"
@@ -13,7 +15,12 @@ MAINNET_WALLET_PATH = "wallets/wallet.json"
 NEOFS_CONTRACT_DEPOSIT_GAS_FEE = 0.1679897
 NEOFS_CONTRACT_WITHDRAW_GAS_FEE = 0.0382514
 NEOFS_CREATE_CONTAINER_GAS_FEE = -1e-08
-NEOFS_EPOCH_TIMEOUT = os.getenv("NEOFS_IR_TIMERS_EPOCH", "300s")
+# NEOFS_EPOCH_TIMEOUT can be declared from neofs-dev-env env variables as NEOFS_IR_TIMERS_EPOCH 
+# (high priority is accepted for env as NEOFS_EPOCH_TIMEOUT)
+if "NEOFS_EPOCH_TIMEOUT" in os.environ:
+    NEOFS_EPOCH_TIMEOUT = os.getenv("NEOFS_EPOCH_TIMEOUT")
+else: 
+    NEOFS_EPOCH_TIMEOUT = os.getenv("NEOFS_IR_TIMERS_EPOCH", "300s")
 BASENET_BLOCK_TIME = os.getenv('chain_SecondsPerBlock', "15s")
 BASENET_WAIT_TIME = "1min"
 MORPH_BLOCK_TIME = os.getenv("morph_SecondsPerBlock", '1s')
@@ -28,14 +35,30 @@ COMPLEX_OBJ_SIZE = 70000000
 #!/usr/bin/python3
 NEOFS_ENDPOINT = "s01.neofs.devenv:8080"
 NEOGO_CLI_PREFIX = "docker exec -it main_chain neo-go"
-NEO_MAINNET_ENDPOINT = os.getenv("NEOFS_IR_MAINNET_ENDPOINT_CLIENT", 'http://main_chain.neofs.devenv:30333')
+# NEO_MAINNET_ENDPOINT can be declared from neofs-dev-env env variables as NEOFS_IR_MAINNET_ENDPOINT_CLIENT 
+# (high priority is accepted for env as NEO_MAINNET_ENDPOINT)
+if "NEO_MAINNET_ENDPOINT" in os.environ:
+    NEO_MAINNET_ENDPOINT = os.getenv("NEO_MAINNET_ENDPOINT")
+else: 
+    NEO_MAINNET_ENDPOINT = os.getenv("NEOFS_IR_MAINNET_ENDPOINT_CLIENT", 'http://main_chain.neofs.devenv:30333')
 
-NEOFS_NEO_API_ENDPOINT = os.getenv("NEOFS_IR_MORPH_ENDPOINT_CLIENT", 'http://morph_chain.neofs.devenv:30333')
+# NEOFS_NEO_API_ENDPOINT can be declared from neofs-dev-env env variables as NEOFS_IR_MORPH_ENDPOINT_CLIENT 
+# (high priority is accepted for env as NEOFS_NEO_API_ENDPOINT)
+if "NEOFS_NEO_API_ENDPOINT" in os.environ:
+    NEOFS_NEO_API_ENDPOINT = os.getenv("NEOFS_NEO_API_ENDPOINT")
+else: 
+    NEOFS_NEO_API_ENDPOINT = os.getenv("NEOFS_IR_MORPH_ENDPOINT_CLIENT", 'http://morph_chain.neofs.devenv:30333')
 HTTP_GATE = 'http://http.neofs.devenv'
 S3_GATE = 'https://s3.neofs.devenv:8080'
 NEOFS_NETMAP = ['s01.neofs.devenv:8080', 's02.neofs.devenv:8080','s03.neofs.devenv:8080','s04.neofs.devenv:8080']
 
 GAS_HASH = '0xd2a4cff31913016155e38e474a2c06d08be276cf'
-NEOFS_CONTRACT = os.getenv("NEOFS_IR_CONTRACTS_NEOFS", '1e6d8b8e1a7c976649dc630062d8b281cb9c2615')
+
+# NEOFS_CONTRACT can be declared from neofs-dev-env env variables as NEOFS_IR_CONTRACTS_NEOFS 
+# (high priority is accepted for env as NEOFS_CONTRACT)
+if "NEOFS_CONTRACT" in os.environ:
+    NEOFS_CONTRACT = os.getenv("NEOFS_CONTRACT")
+else: 
+    NEOFS_CONTRACT = os.getenv("NEOFS_IR_CONTRACTS_NEOFS", '1e6d8b8e1a7c976649dc630062d8b281cb9c2615')
 DEF_WALLET_ADDR = "NVUzCUvrbuWadAm6xBoyZ2U7nCmS9QBZtb"
 TEMP_DIR = "TemporaryDir/"
