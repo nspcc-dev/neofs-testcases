65f76288792bb0615e2fb21fc2bdcc2f54b98fe7
EliChin <70199136+EliChin@users.noreply.github.com>
2021-04-19 19:45:42 +0300

    Update common.py

--- robot/variables/common.py
+++ robot/variables/common.py
@@ -5,7 +5,6 @@ ROOT='../..'
 RESOURCES="%s/resources/lib" % ROOT
 CERT="%s/../../ca" % ROOT
 
-os.environ
 # path from repo root is required for object put and get
 # in case when test is run from root in docker
 ABSOLUTE_FILE_PATH="/robot/testsuites/integration"
