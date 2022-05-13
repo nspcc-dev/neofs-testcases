5d3b40db07adf41eee30e3731a041ac72d5ea926
anastasia prasolova <anastasia@nspcc.ru>
2022-05-13 14:36:21 +0300

    python3.8 -> python3.9 in venv.localtest
    Signed-off-by: anastasia prasolova <anastasia@nspcc.ru>

--- venv_template.mk
+++ venv_template.mk
@@ -3,11 +3,11 @@ venv.$(1): venv.$(1)/bin/activate venv.$(1)/bin/environment.sh
 
 venv.$(1)/bin/activate: venv/$(1)/requirements.txt submodules
 	@echo "Creating $(1) venv in $$@ from $$<"
-	virtualenv --python=python3.8 --prompt="($(1))" venv.$(1)
+	virtualenv --python=python3.9 --prompt="($(1))" venv.$(1)
 	source venv.$(1)/bin/activate && \
-	pip3.8 install -U setuptools==56.0.0 && \
-	pip3.8 install -Ur venv/$(1)/requirements.txt && \
-	pip3.8 install -Ur neofs-keywords/requirements.txt
+	pip3.9 install -U setuptools==56.0.0 && \
+	pip3.9 install -Ur venv/$(1)/requirements.txt && \
+	pip3.9 install -Ur neofs-keywords/requirements.txt
 	@echo "Applying activate script patch"
 	patch -R --dry-run -p1 -s -f -d venv.$(1)/bin/ < build_assets/activate.patch || \
 	patch -p1 -d venv.$(1)/bin/ < build_assets/activate.patch
