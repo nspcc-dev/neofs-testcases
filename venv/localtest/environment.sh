b6cbd7c07c469641ed54e49072f8cefaf4a66bdb
anastasia prasolova <anastasia@nspcc.ru>
2022-08-17 13:06:44 +0300

    removed robot tests
    Signed-off-by: anastasia prasolova <anastasia@nspcc.ru>

--- venv/localtest/environment.sh
+++
@@ -1,8 +0,0 @@
-# DevEnv variables
-export NEOFS_MORPH_DISABLE_CACHE=true
-export DEVENV_PATH="${DEVENV_PATH:-${VIRTUAL_ENV}/../../neofs-dev-env}"
-pushd $DEVENV_PATH > /dev/null
-export `make env`
-popd > /dev/null
-
-export PYTHONPATH=${PYTHONPATH}:${VIRTUAL_ENV}/../neofs-keywords/lib:${VIRTUAL_ENV}/../neofs-keywords/robot:${VIRTUAL_ENV}/../robot/resources/lib/python_keywords:${VIRTUAL_ENV}/../robot/resources/lib/robot:${VIRTUAL_ENV}/../robot/variables
