f80a9b7cbef60263fa97cf73c8bdc9d079423e7b
Andrey Berezin <a.berezin@yadro.com>
2022-11-01 19:11:26 +0300

    Refactor Api tests and extend get_ranges_tests
    Signed-off-by: Andrey Berezin <a.berezin@yadro.com>

--- pytest_tests/helpers/grpc_responses.py
+++ pytest_tests/helpers/grpc_responses.py
@@ -9,6 +9,7 @@ OBJECT_ACCESS_DENIED = "code = 2048.*message = access to object operation denied
 OBJECT_NOT_FOUND = "code = 2049.*message = object not found"
 OBJECT_ALREADY_REMOVED = "code = 2052.*message = object already removed"
 SESSION_NOT_FOUND = "code = 4096.*message = session token not found"
+OUT_OF_RANGE = "code = 2053.*message = out of range"
 
 
 def error_matches_status(error: Exception, status_pattern: str) -> bool:
