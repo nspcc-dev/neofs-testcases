764199192eb0d4fb94a01d242d9bb98d7ddc6e87
Мария Малыгина <m.malygina@MacBook-Pro-Maria.local>
2023-01-13 16:08:53 +0300

    fix list len
    Signed-off-by: Мария Малыгина <m.malygina@MacBook-Pro-Maria.local>

--- pytest_tests/testsuites/load/test_load.py
+++ pytest_tests/testsuites/load/test_load.py
@@ -61,7 +61,7 @@ class TestLoad(ClusterTestBase):
                 hosting=hosting,
                 container_placement_policy=CONTAINER_PLACEMENT_POLICY,
                 region=REGION,
-                node_amount=len(STORAGE_NODE_IPS.len)
+                node_amount=len(STORAGE_NODE_IPS)
             )
 
     @pytest.mark.parametrize("obj_size, out_file", list(zip(OBJ_SIZE, OUT_FILE)))
