2102a452ad97dacf9543d5147573dfff3f68c7a3
Oleg Kulachenko <oleg@nspcc.ru>
2023-04-13 20:04:51 +0400

    Change venv
    Signed-off-by: Oleg Kulachenko <oleg@nspcc.ru>

--- venv_template.mk
+++ venv_template.mk
@@ -6,8 +6,8 @@ venv.$(1)/bin/activate:
 	virtualenv --python=python3.10 --prompt="($(1))" venv.$(1)
 	. venv.$(1)/bin/activate && \
 	python -m pip install --upgrade pip && \
-	pip3.10 install -U setuptools==56.0.0 && \
-	pip3.10 install -Ur requirements.txt
+	pip install -U setuptools==56.0.0 && \
+	pip install -Ur requirements.txt
 	@echo "Applying activate script patch"
 	patch -R --dry-run -p1 -s -f -d venv.$(1)/bin/ < build_assets/activate.patch || \
 	patch -p1 -d venv.$(1)/bin/ < build_assets/activate.patch
