b3dd3e547b4eddf61e66233a13f4f716b6779383
Oleg Kulachenko <oleg@nspcc.ru>
2023-09-14 17:56:42 +0000

    helpers: Separate eACL grps responses
    Signed-off-by: Oleg Kulachenko <oleg@nspcc.ru>

--- pytest_tests/helpers/grpc_responses.py
+++ pytest_tests/helpers/grpc_responses.py
@@ -30,6 +30,7 @@ NOT_CONTAINER_OWNER = "provided account differs with the container owner"
 NOT_SESSION_CONTAINER_OWNER = "session issuer differs with the container owner"
 TIMED_OUT = "timed out after \\d+ seconds"
 CONTAINER_DELETION_TIMED_OUT = "container deletion: await timeout expired"
+
 EACL_TIMED_OUT = "eACL setting: await timeout expired"
 EACL_TABLE_IS_NOT_SET = "extended ACL table is not set for this container"
 EACL_NOT_FOUND = "code = 3073.*message = eACL not found"
