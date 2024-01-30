3202e67951fde843892ba23c6e85d46c6bba69ce
Oleg Kulachenko <oleg@nspcc.ru>
2024-01-30 22:21:06 +0300

    Replace creating wallets
    Replace creating wallets from session token to root conftest.
    
    Signed-off-by: Oleg Kulachenko <oleg@nspcc.ru>

--- dynamic_env_pytest_tests/tests/conftest.py
+++ dynamic_env_pytest_tests/tests/conftest.py
@@ -12,11 +12,51 @@ from common import (
     TEST_FILES_DIR,
     TEST_OBJECTS_DIR,
 )
-from neofs_testlib.env.env import NeoFSEnv
+from neofs_testlib.env.env import NeoFSEnv, NodeWallet
 from neofs_testlib.shell import Shell
 from python_keywords.neofs_verbs import get_netmap_netinfo
 
 from helpers.wallet_helpers import create_wallet
+
+
+@pytest.fixture(scope="module")
+def owner_wallet(temp_directory) -> NodeWallet:
+    """
+    Returns wallet which owns containers and objects
+    """
+    return create_wallet()
+
+
+@pytest.fixture(scope="module")
+def user_wallet(temp_directory) -> NodeWallet:
+    """
+    Returns wallet which will use objects from owner via static session
+    """
+    return create_wallet()
+
+
+@pytest.fixture(scope="module")
+def stranger_wallet(temp_directory) -> NodeWallet:
+    """
+    Returns stranger wallet which should fail to obtain data
+    """
+    return create_wallet()
+
+
+@pytest.fixture(scope="module")
+def scammer_wallet(temp_directory) -> NodeWallet:
+    """
+    Returns stranger wallet which should fail to obtain data
+    """
+    return create_wallet()
+
+
+@pytest.fixture(scope="module")
+def not_owner_wallet(temp_directory) -> NodeWallet:
+    """
+    Returns stranger wallet which should fail to obtain data
+    """
+    return create_wallet()
 
 
 def pytest_addoption(parser):
