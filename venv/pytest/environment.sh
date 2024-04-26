bf191fc56e8208ace1971255fdeaec2176563246
Evgeniy Zayats <zayatsevgeniy@nspcc.io>
2024-04-25 20:25:24 -0400

    .venv: remove useless popd from environment.sh
    Signed-off-by: Evgeniy Zayats <zayatsevgeniy@nspcc.io>

--- venv/pytest/environment.sh
+++ venv/pytest/environment.sh
@@ -1,6 +1,5 @@
 # DevEnv variables
 export NEOFS_MORPH_DISABLE_CACHE=true
 export WALLET_PASS=password
-popd > /dev/null
 
 export PYTHONPATH=${PYTHONPATH}:${VIRTUAL_ENV}/../pytest_tests/lib:${VIRTUAL_ENV}/../neofs-testlib/
