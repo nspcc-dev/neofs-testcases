ee7d721dcbd31b76297c33e9ef936293b35779c9
Evgeniy Zayats <zayatsevgeniy@nspcc.io>
2024-11-16 15:38:42 -0500

    tests: add positive tests for s3 presigned urls
    closes #241
    
    Signed-off-by: Evgeniy Zayats <zayatsevgeniy@nspcc.io>

--- neofs-testlib/neofs_testlib/cli/neofs_authmate/authmate.py
+++ neofs-testlib/neofs_testlib/cli/neofs_authmate/authmate.py
@@ -2,13 +2,16 @@ from typing import Optional
 
 from neofs_testlib.cli.neofs_authmate.secret import NeofsAuthmateSecret
 from neofs_testlib.cli.neofs_authmate.version import NeofsAuthmateVersion
+from neofs_testlib.cli.neofs_authmate.presigned import NeofsAuthmatePresigned
 from neofs_testlib.shell import Shell
 
 
 class NeofsAuthmate:
     secret: Optional[NeofsAuthmateSecret] = None
     version: Optional[NeofsAuthmateVersion] = None
+    presigned: Optional[NeofsAuthmatePresigned] = None
 
     def __init__(self, shell: Shell, neofs_authmate_exec_path: str):
         self.secret = NeofsAuthmateSecret(shell, neofs_authmate_exec_path)
         self.version = NeofsAuthmateVersion(shell, neofs_authmate_exec_path)
+        self.presigned = NeofsAuthmatePresigned(shell, neofs_authmate_exec_path)
