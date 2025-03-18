3c0cca8679724ea6bfb1c005fef03cf885314d14
Evgeniy Zayats <zayatsevgeniy@nspcc.io>
2025-03-18 13:57:31 -0400

    tests: add test for get request forwarding
    closes #448
    
    Signed-off-by: Evgeniy Zayats <zayatsevgeniy@nspcc.io>

--- pytest_tests/tests/failovers/test_failover_storage.py
+++ pytest_tests/tests/failovers/test_failover_storage.py
@@ -1,12 +1,24 @@
 import logging
+import queue
 import random
+import threading
+import time
 
 import allure
 import pytest
-from helpers.complex_object_actions import wait_object_replication
+from helpers.complex_object_actions import (
+    get_nodes_with_object,
+    get_nodes_without_object,
+    wait_object_replication,
+)
 from helpers.container import create_container
 from helpers.file_helper import generate_file, get_file_hash
-from helpers.neofs_verbs import get_object, put_object, put_object_to_random_node
+from helpers.neofs_verbs import (
+    get_netmap_netinfo,
+    get_object,
+    put_object,
+    put_object_to_random_node,
+)
 from helpers.node_management import storage_node_healthcheck, wait_all_storage_nodes_returned
 from helpers.wellknown_acl import PUBLIC_ACL
 from neofs_testlib.env.env import NeoFSEnv, StorageNode
@@ -185,3 +197,91 @@ class TestFailoverStorage:
 
         with allure.step("Return stopped storage node"):
             self.return_stopped_storage_nodes(self.neofs_env, dead_nodes)
+
+    def test_get_forwarding(
+        self,
+        default_wallet,
+        neofs_env_function_scope: NeoFSEnv,
+        after_run_return_all_stopped_storage_nodes,
+    ):
+        self.neofs_env = neofs_env_function_scope
+        self.shell = self.neofs_env.shell
+
+        with allure.step("Create container"):
+            wallet = default_wallet
+            placement_rule = "REP 2 IN X CBF 1 SELECT 2 FROM * AS X"
+            cid = create_container(
+                wallet.path,
+                shell=self.shell,
+                endpoint=self.neofs_env.storage_nodes[0].endpoint,
+                rule=placement_rule,
+                basic_acl=PUBLIC_ACL,
+            )
+
+        with allure.step("Put large object"):
+            storage_node = self.neofs_env.storage_nodes[0]
+            net_info = get_netmap_netinfo(
+                wallet=storage_node.wallet.path,
+                wallet_config=storage_node.cli_config,
+                endpoint=storage_node.endpoint,
+                shell=self.shell,
+            )
+            large_file_size = net_info["maximum_object_size"]
+            source_file_path = generate_file(large_file_size)
+            oid = put_object(
+                wallet.path,
+                source_file_path,
+                cid,
+                shell=self.shell,
+                endpoint=self.neofs_env.storage_nodes[0].endpoint,
+            )
+
+        with allure.step("Get info about nodes with object"):
+            nodes_with_object = get_nodes_with_object(
+                cid,
+                oid,
+                shell=self.shell,
+                nodes=self.neofs_env.storage_nodes,
+                neofs_env=self.neofs_env,
+            )
+
+            nodes_without_object = get_nodes_without_object(
+                wallet.path,
+                cid,
+                oid,
+                shell=self.shell,
+                nodes=self.neofs_env.storage_nodes,
+            )
+
+        with allure.step("Send get request to a random node without object and turn off a node with object"):
+            exception_queue = queue.Queue()
+
+            def get_object_from_node():
+                for _ in range(5):
+                    try:
+                        logger.info(f"{nodes_without_object=}")
+                        got_file_path = get_object(
+                            wallet.path,
+                            cid,
+                            oid,
+                            shell=self.shell,
+                            endpoint=random.choice(nodes_without_object).endpoint,
+                        )
+                        assert get_file_hash(source_file_path) == get_file_hash(got_file_path), "Invalid file cache"
+                    except Exception as e:
+                        exception_queue.put(e)
+                    time.sleep(2)
+
+            get_thread = threading.Thread(target=get_object_from_node)
+            get_thread.start()
+
+            for _ in range(2):
+                node_to_kill = random.choice(nodes_with_object)
+                node_to_kill.kill()
+                node_to_kill.start(fresh=False)
+                wait_all_storage_nodes_returned(self.neofs_env)
+
+            get_thread.join()
+            if not exception_queue.empty():
+                exc = exception_queue.get()
+                raise AssertionError(f"Exception in thread: {exc}")
