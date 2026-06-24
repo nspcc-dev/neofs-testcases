8e41654685e03f70d84e7666fe5091a324c3d581
Evgeniy Zayats <zayatsevgeniy@nspcc.io>
2026-06-24 12:29:56 -0400

    tests: fix test_sn_deployment_with_writecache
    Signed-off-by: Evgeniy Zayats <zayatsevgeniy@nspcc.io>

--- pytest_tests/tests/network/test_deployment.py
+++ pytest_tests/tests/network/test_deployment.py
@@ -147,6 +147,11 @@ def test_sn_deployment_with_writecache(neofs_env_with_writecache: NeoFSEnv):
                 pprof_address=sn.pprof_address,
                 prometheus_address=sn.prometheus_address,
                 attrs=sn.node_attrs,
+                metadata_path=sn.metadata_path,
+                metadata_legacy_format=sn._metadata_legacy_format(),
+                metadata_seed_port=sn.metadata_seed_port,
+                metadata_p2p_address=sn.metadata_p2p_address,
+                metadata_rpc_address=sn.metadata_rpc_address,
             )
             sn.start(fresh=False)
 
